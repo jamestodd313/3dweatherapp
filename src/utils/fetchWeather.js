47cc1314285f67b8dc14b1e05f3b8f001f7e7d66
jamestodd313 <jamestodd313@gmail.com>
2021-03-17 18:33:22 -0500

    api and fetch function set up

---
+++ src/utils/fetchWeather.js
@@ -0,0 +1,21 @@
+export const fetchWeather = function(query){
+    // const WEATHER_API_KEY = process.env.REACT_APP_WEATHER_API_KEY
+    // return fetch(`https://weatherapi-com.p.rapidapi.com/current.json?q=${query}`, {
+    //   "method": "GET",
+    //   "headers": {
+    //     "x-rapidapi-key": WEATHER_API_KEY,
+    //     "x-rapidapi-host": "weatherapi-com.p.rapidapi.com"
+    //   }
+    // })
+    // .then(response => {
+    //   return response.json()
+    // })
+    // .then(weatherData=>{
+    //   return weatherData
+    // })
+    // .catch(err => {
+    //   console.error(err);
+    // });
+    
+    return {location: {name: 'Minneapolis'}, current: {lastUpdated: 1234564}}
+}
