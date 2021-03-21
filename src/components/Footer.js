a84d2cec0c08d097943b64d782c9b6673747512b
jamestodd313 <jamestodd313@gmail.com>
2021-03-21 16:10:25 -0500

    added credit links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,18 +4,18 @@ export const Footer = () => {
     return (
         <footer>
             <span className="credit-section">
-                <a href="ADD A LINK HERE" className="credit-link" target="_blank" rel="noreferrer noopener">A Windy Day </a>
+                <a href="https://sketchfab.com/3d-models/a-windy-day-fb78f4cc938144e6902dd5cff354d525" className="credit-link" target="_blank" rel="noreferrer noopener">A Windy Day </a>
                 by
-                <a href="ADD A LINK HERE" className="credit-link" target="_blank" rel="noreferrer noopener"> Loïc Norgeot </a>
+                <a href="https://sketchfab.com/norgeotloic" className="credit-link" target="_blank" rel="noreferrer noopener"> Loïc Norgeot </a>
                 is licensed under Creative Commons Attribution
             </span>
             <span className="credit-section">     
                 Data provided by
-                <a href="ADD A LINK HERE" className="credit-link" target="_blank" rel="noreferrer noopener"> WeatherAPI.com </a>
+                <a href="https://www.weatherapi.com" className="credit-link" target="_blank" rel="noreferrer noopener"> WeatherAPI.com </a>
             </span>
             <span className="credit-section">
                 3D Weather App by
-                <a href="ADD A LINK HERE" className="credit-link" target="_blank" rel="noreferrer noopener"> James Todd </a>
+                <a href="https://jamestodd.dev" className="credit-link" target="_blank" rel="noreferrer noopener"> James Todd </a>
             </span>
         </footer>
     )
--- src/components/Model.js
+++ src/components/Model.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import {init} from '../3d/init'
 export const Model = ({condition}) => {
     const [modelToDisplay, setModelToDisplay] = useState(undefined)
+    init()
     return (
         <div className="model-container">
             {modelToDisplay ? modelToDisplay : 'Loading model...'}
